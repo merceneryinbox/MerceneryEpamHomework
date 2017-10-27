a3744aa9e56624b302e3ef8f7a6c6dd107ee72d0
mercenery <mercenery@inbox.ru>
2017-10-27 09:28:58 +0300

    after formatter adjust

--- src/main/java/homework/epam/mod4/task1byteIO/MerceneryJavaReservedWordsFileParsser.java
+++ src/main/java/homework/epam/mod4/task1byteIO/MerceneryJavaReservedWordsFileParsser.java
@@ -1,4 +1,42 @@
 package homework.epam.mod4.task1byteIO;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MerceneryJavaReservedWordsFileParsser {
+	private static String reservedJavaWordsFileName;
+	private static String outputFileName;
+	private static String javaFileName;
+	private static Map<String, Integer> resultMap = new HashMap<>();
+	private static List<String>  javaKeyWordsInFile;
+	private static List<Pattern> keyWordsPatterns;
+	private static Matcher       matcher;
+	byte[] bytesFromReservedWordFile;
+	byte[] bytesFromJavaFile;
+	private StringBuilder textFromJavaFile = null;
+	private String result;
+	
+	
+	public MerceneryJavaReservedWordsFileParsser() {
+	}
+	
+	
+	public Map transmitJavaWordCount(String reservedJavaWordsFileName, String outputFileName, String javaFileName) {
+		try (OutputStream outStreamReservedWords = Files.newOutputStream(Paths.get(reservedJavaWordsFileName));
+		     OutputStream outJavaFile = Files.newOutputStream(Paths.get(javaFileName));
+		     InputStream fileToWriteTo = Files.newInputStream(Paths.get(outputFileName))) {
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return resultMap;
+	}
 }
--- src/main/java/homework/epam/mod4/task1byteIO/SE04JavaByteSpecWordsParser.java
+++ src/main/java/homework/epam/mod4/task1byteIO/SE04JavaByteSpecWordsParser.java
@@ -1,4 +1,31 @@
+/*
+*Задание 1. Работа с байтовыми потоками ввода-вывода
+1) Прочитайте файл, содержащий код на языке Java.(https://habrahabr.ru/post/108076/ , https://ru.stackoverflow.com/questions/429706/%D0%92-%D0%BA%D0%B0%D0%BA%D0%B8%D1%85-%D1%81%D0%BB%D1%83%D1%87%D0%B0%D1%8F%D1%85-%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-bytearrayinputstream)
+2) Определите, какие ключевые слова языка Java это код содержит.
+3) Выведите эти слова и их количество в другой файл.
+-- !-- Используйте только байтовые потоки ввода-вывода.-- !--
+* */
 package homework.epam.mod4.task1byteIO;
 
+import java.util.Map;
+
 public class SE04JavaByteSpecWordsParser {
+    String[] reservedJavaWords;
+
+    public static void main(String[] args) {
+        // declare & initiate filenames
+        String reservedJavaWordsFileName = "javareservedwords.txt";
+        String javaFileName = MerceneryJavaReservedWordsFileParsser.class.getName();
+        String outputFileName = "outputFile.txt";
+
+        // initiate my parserPlus class object
+        MerceneryJavaReservedWordsFileParsser parserPlus = new MerceneryJavaReservedWordsFileParsser();
+
+        // store map with keywords & theirs count
+        Map<String, Integer> resultInformation = parserPlus.transmitJavaWordCount(reservedJavaWordsFileName, outputFileName, javaFileName);
+        for (Map.Entry entry : resultInformation.entrySet()) {
+            String tmpStr = "В файле " + javaFileName + " встречается " + entry.getValue() + " ключевое слово " + entry.getKey();
+            System.out.println(tmpStr);
+        }
+    }
 }
