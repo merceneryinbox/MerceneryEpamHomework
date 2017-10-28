e2f18679322220397f4d108f61ba2fd2ae42b345
mercenery <mercenery@inbox.ru>
2017-10-28 04:23:08 +0300

    task 1 SE04 add arrays patterns and strings

--- src/main/java/homework/epam/mod4/task1byteIO/MerceneryJavaReservedWordsFileParser.java
+++ src/main/java/homework/epam/mod4/task1byteIO/MerceneryJavaReservedWordsFileParser.java
@@ -2,8 +2,6 @@ package homework.epam.mod4.task1byteIO;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -41,15 +39,15 @@ public class MerceneryJavaReservedWordsFileParser {
 			
 			reservedJavaWordsFile.read(bytesFromReservedWordFileArray, 0, bytesFromReservedWordFileArray.length);
 			javaFile.read(bytesFromJavaFileArray, 0, bytesFromJavaFileArray.length);
-			
-// create array with java keywords from byte[]->String->String[] via split() for fulfilling Pattern array
+
+// retrieve arrays of bytes from files - with java keywords and with .java program file byte[]->String->String[] via split()
 			reservedWordsInString = new String(bytesFromReservedWordFileArray, StandardCharsets.UTF_8);
 			javaReservedWordsInFileArray = reservedWordsInString.split("\\n");
 			keyWordsPatternsArray = new Pattern[javaReservedWordsInFileArray.length];
 			for (int i = 0; i < javaReservedWordsInFileArray.length; i++) {
 				keyWordsPatternsArray[i] = Pattern.compile(javaReservedWordsInFileArray[i]);
 			}
-			
+
 // create byte[]->String->String[] via split() separating all words in file to alone String from *.java file
 			javaFileInString = new String(bytesFromJavaFileArray, StandardCharsets.UTF_8);
 			javaFileInStringArray = javaFileInString.split("\\s,\\n,\\t,^[a-zA-Z]|^[0-9]");
@@ -58,43 +56,6 @@ public class MerceneryJavaReservedWordsFileParser {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		;
-		InputStream javaFileRead = new ByteArrayInputStream(Files.readAllBytes(Paths.get(javaFileName)))){
-
-// retrieve arrays of bytes from files - with java keywords and with .java program file
-			bytesFromReservedWordFileArray = reservedJavaWordsFile.readAllBytes();
-			bytesFromJavaFileArray = javaFileRead.readAllBytes();
-
-// retrieve string with java key words and java program file
-			reservedWordsInString = new String(bytesFromReservedWordFileArray, StandardCharsets.UTF_8);
-			javaFileInString = new String(bytesFromJavaFileArray, StandardCharsets.UTF_8);
-
-// retrieve array of java keywords from string witch retrieved from file, using new line splitter and initializingmPattern Array.
-			javaReservedWordsInFileArray = reservedWordsInString.split("\\n");
-			keyWordsPatternsArray = new Pattern[javaReservedWordsInFileArray.length];
-			javaFileInStringArray = javaFileInString.split("\\s|\\n|^[a-zA-Z]|^[0-9]");
-
-// store all keywords in the Map with count of inclusive equals 0 and fulfilling array of Patterns for Matcher
-			for (int i = 0; i < javaReservedWordsInFileArray.length; i++) {
-				resultMap.put(javaReservedWordsInFileArray[i], 0);
-				keyWordsPatternsArray[i] = Pattern.compile(javaReservedWordsInFileArray[i]);
-			}
-
-// let's parse java keywords in javafile and count its inclusives
-			for (int j = 0; j < keyWordsPatternsArray.length; j++) {
-				String tmpStr  = javaReservedWordsInFileArray[j];
-				int    counter = 0;
-				for (int k = 0; k < javaFileInStringArray.length; k++) {
-					if (tmpStr.equals(javaFileInStringArray[k])) {
-						counter++;
-					}
-					resultMap.put(tmpStr, counter);
-				}
-			}
-			
-		} catch(IOException e){
 			e.printStackTrace();
 		}
 		return resultMap;
