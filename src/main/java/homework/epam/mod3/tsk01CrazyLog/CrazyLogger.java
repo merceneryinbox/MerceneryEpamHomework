8821d08bcbefdef89fc79b59be385b74d76a7c04
mercenery <mercenery@inbox.ru>
2017-11-09 12:03:13 +0300

    SE03 tsk01 CrazyLogger Complete

---
+++ src/main/java/homework/epam/mod3/tsk01CrazyLog/CrazyLogger.java
@@ -0,0 +1,125 @@
+package homework.epam.mod3.tsk01CrazyLog;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CrazyLogger {
+	static private volatile StringBuilder accumExceptionSTraceBuild = null;
+	static private volatile StringBuilder accumExceptionInfo        = null;
+	static private volatile boolean       consoleLogOn              = false;
+	static private volatile boolean       fileLogOn                 = false;
+	static private volatile String[] poolOfTraceInStringsAr;
+	static private volatile String crazyLogFileName = "craxyLogFile.log";
+	static private Exception incomeException;
+	static private LocalDate localDate;
+	static private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-mm-YYYY : hh-mm - ");
+	
+	public CrazyLogger() {
+		localDate = LocalDate.now();
+		localDate.format(dateTimeFormatter);
+	}
+	
+	public static boolean isConsoleLogOn() {
+		return consoleLogOn;
+	}
+	
+	public static void setConsoleLogOn(boolean consoleLogOn) {
+		CrazyLogger.consoleLogOn = consoleLogOn;
+	}
+	
+	public boolean showMeLogBuilderInConsole() {
+		boolean result = false;
+		if (consoleLogOn) {
+			System.out.println(localDate.toString() + accumExceptionSTraceBuild);
+		}
+		System.out.println("Console logging - OFF");
+		return result;
+	}
+	
+	public void takeException(Exception incomeException) {
+		this.incomeException = incomeException;
+		StackTraceElement[] poolOfST = incomeException.getStackTrace();
+		poolOfTraceInStringsAr = new String[poolOfST.length];
+		
+		for (int i = 0; i < poolOfTraceInStringsAr.length; i++) {
+			poolOfTraceInStringsAr[i] = localDate.toString() +
+			                            poolOfST[i].getLineNumber() + " - " + poolOfST[i].getClassName()
+			                            + " in method - "
+			                            + poolOfST[i].getMethodName() + " " + poolOfST[i].getFileName();
+		}
+		
+		for (int i = 0; i < poolOfTraceInStringsAr.length; i++) {
+			accumExceptionSTraceBuild.append(poolOfTraceInStringsAr[i]);
+		}
+	}
+	
+	public boolean saveToCustomLogToFile(String customLogFName) {
+		boolean result = false;
+		if (isFileLogOn()) {
+			try (FileOutputStream fileOutputStream = new FileOutputStream(Paths.get(customLogFName)
+					                                                              .toFile())) {
+				char[] chars = poolOfTraceInStringsAr.toString()
+						.toCharArray();
+				byte[] bytes = new byte[chars.length];
+				
+				for (int i = 0; i < chars.length; i++) {
+					bytes[i] = (byte) chars[i];
+				}
+				fileOutputStream.write(bytes);
+				result = true;
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public static boolean isFileLogOn() {
+		return fileLogOn;
+	}
+	
+	public static void setFileLogOn(boolean fileLogOn) {
+		CrazyLogger.fileLogOn = fileLogOn;
+	}
+	
+	public boolean saveSilentLogToFile() {
+		boolean result = false;
+		
+		if (isFileLogOn()) {
+			try (FileOutputStream fileOutputStream = new FileOutputStream(Paths.get(crazyLogFileName)
+					                                                              .toFile())) {
+				char[] chars = poolOfTraceInStringsAr.toString()
+						.toCharArray();
+				byte[] bytes = new byte[chars.length];
+				
+				for (int i = 0; i < chars.length; i++) {
+					bytes[i] = (byte) chars[i];
+				}
+				fileOutputStream.write(bytes);
+				result = true;
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public String giveMeInfoAbout(String findInLog) {
+		String resultString = "Nothing is found";
+		for (int i = 0; i < poolOfTraceInStringsAr.length; i++) {
+			if (poolOfTraceInStringsAr[i].contains(findInLog)) {
+				accumExceptionInfo.append(poolOfTraceInStringsAr[i]);
+			}
+			resultString = accumExceptionInfo.toString();
+		}
+		return resultString;
+	}
+}
