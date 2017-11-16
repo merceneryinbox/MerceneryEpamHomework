af2afec71fcee2b461919ea3b783d449d247a260
mercenery <mercenery@inbox.ru>
2017-11-17 00:12:57 +0300

    se08 tsk01 DB work

--- src/main/java/homework/epam/mod08/tsk01DBTest/se08tsk01DB.java
+++ src/main/java/homework/epam/mod08/tsk01DBTest/se08tsk01DB.java
@@ -1,26 +1,64 @@
 package homework.epam.mod08.tsk01DBTest;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class se08tsk01DB {
-	private static final Properties CONFIGS;
+	private static Properties CONFIGS;
 	private static Connection connection;
-	static {
-		CONFIGS = new Properties();
-		try (InputStream propertyStream = new FileInputStream("./src/resources/se08tsk01DBPGconfigs.xml")) {
-			CONFIGS.loadFromXML(propertyStream);
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
-	}
 	
 	public static void main(String[] args) {
-		connection = ;
+		try (InputStream propertyStream = new FileInputStream("./src/resources/se08tsk01DBPGconfigs.xml")) {
+			CONFIGS.loadFromXML(propertyStream);
+			Class.forName(getDBDriver());
+			connection = DriverManager.getConnection(getUrl(), "username", "password");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	private static String getDBDriver() {
+		return CONFIGS.getProperty("database.driver");
+	}
+	
+	private static String getUrl() {
+		return getProtocol() + "://" + getHost() + ":" + getPort() + "/" +
+		       getDatabaseName();
+	}
+	
+	private static String getProtocol() {
+		return CONFIGS.getProperty("database.driver");
+	}
+	
+	private static String getHost() {
+		return CONFIGS.getProperty("database.host");
+	}
+	
+	private static String getPort() {
+		return CONFIGS.getProperty("database.port");
+	}
+	
+	private static String getDatabaseName() {
+		return CONFIGS.getProperty("database.name");
 	}
 	
 	private String getUser() {
@@ -29,26 +67,5 @@ public class se08tsk01DB {
 	
 	private String getPassword() {
 		return CONFIGS.getProperty("database.password");
-	}
-	
-	private String getUrl() {
-		return getProtocol() + "://" + getHost() + ":" + getPort() + "/" +
-		       getDatabaseName();
-	}
-	
-	private String getProtocol() {
-		return CONFIGS.getProperty("database.driver");
-	}
-	
-	private String getHost() {
-		return CONFIGS.getProperty("database.host");
-	}
-	
-	private String getPort() {
-		return CONFIGS.getProperty("database.port");
-	}
-	
-	private String getDatabaseName() {
-		return CONFIGS.getProperty("database.name");
 	}
 }
