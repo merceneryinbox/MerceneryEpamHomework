2fec11adff19dc27b8271febae0c73874ba63f22
mercenery <mercenery@inbox.ru>
2017-11-16 21:05:06 +0300

    se08 tsk01 work

---
+++ src/resources/SE08tsk01installDB.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA se08tsk01schema;
+
+CREATE DATABASE se08tsk01dbEpamHW;
+
+DROP  TABLE users_table;
+
+CREATE TABLE users_table();
+
+ALTER TABLE public.users_table ADD ID_USER SERIAL NOT NULL;
