ab02d67c9aba29cf393e6c526e3a6e1ebf5b1202
mercenery <mercenery@inbox.ru>
2017-11-16 20:44:18 +0300

    se08 tsk01 start

---
+++ src/main/java/homework/epam/mod08/tsk01DBTest/se08tsk01DB.java
@@ -0,0 +1,13 @@
+package homework.epam.mod08.tsk01DBTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class se08tsk01DB {
+	DriverManager pgDriverManager;
+	Connection connection;
+	String url = ;
+	
+	public static void main(String[] args) {
+	}
+}
