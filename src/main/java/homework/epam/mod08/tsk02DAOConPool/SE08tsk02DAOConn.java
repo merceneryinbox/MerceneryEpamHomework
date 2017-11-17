960df28248000cc38e7fe9b5acac849d9555b3d8
mercenery <mercenery@inbox.ru>
2017-11-17 10:51:25 +0300

    se08 tsk02 DB work

--- src/main/java/homework/epam/mod08/tsk02DAOConPool/SE08tsk02DAOConn.java
+++
@@ -1,7 +0,0 @@
-package homework.epam.mod08.tsk02DAOConPool;
-
-public class SE08tsk02DAOConn {
-	public static void main(String[] args) {
-	
-	}
-}
---
+++ src/main/java/homework/epam/mod08/tsk02DAOConPool/SE08tsk02HomeLibDAOConn.java
@@ -0,0 +1,15 @@
+package homework.epam.mod08.tsk02DAOConPool;
+
+/*
+Задание 2. DAO&ConnectionPool
+Спроектируйте БД, хранящую информацию, например, о домашней библиотеке.
+Реализуйте функциональность добавления, поиска и удаления разнообразной информации
+из этой БД. При реализации используйте (напишите) пул соединений и DAO.
+ */
+public class SE08tsk02HomeLibDAOConn {
+	public static void main(String[] args) {
+		HomeLibDAOManager homeLib = new HomeLibDAOManager();
+		
+		//
+	}
+}
